f5664c6b26aa2394d22bab5969f98972c79e0e10
Jason <jasonw2910@gmail.com>
2023-03-08 01:43:58 -0500

    Add signup capabilities

--- restify/accounts/models.py
+++ restify/accounts/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 class User(AbstractUser):
     """A custom User model for ease of customization."""
-    phone_number = models.CharField(max_length=10)
+    username = None
+    first_name = models.CharField("first name", max_length=150)
+    last_name = models.CharField("last name", max_length=150)
+    email = models.EmailField("email address", unique=True)
+    phone_number = models.CharField(max_length=20)
     avatar = models.ImageField(default='default_avatar.png')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
--- restify/accounts/views.py
+++ restify/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+
+from accounts.models import User
+from accounts.serializers import UserCreationSerializer
+
 
 # Create your views here.
+class CreateUser(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreationSerializer
+
+
+
