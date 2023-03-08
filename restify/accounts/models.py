88fb092002cc7fb0825d72102cb886f1f1c27466
Kevin Wang <76802978+SirRAL@users.noreply.github.com>
2023-03-08 18:44:27 -0500

    Make superusers not require usernames

--- restify/accounts/models.py
+++ restify/accounts/models.py
@@ -1,10 +1,37 @@
+from django.contrib.auth.hashers import make_password
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    """
+    A custom UserManager that has no requirement for a username to be present.
+    """
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = User(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        assert extra_fields['is_staff']
+        assert extra_fields['is_superuser']
+        return self._create_user(email, password, **extra_fields)
 
 
 # Create your models here.
 class User(AbstractUser):
     """A custom User model for ease of customization."""
+    objects = CustomUserManager()
     username = None
     first_name = models.CharField("first name", max_length=150)
     last_name = models.CharField("last name", max_length=150)
