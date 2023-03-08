b4983088bcb734667c16779b0b20a6da8b45e5b3
Jason <jasonw2910@gmail.com>
2023-03-08 14:37:06 -0500

    Add view profile capabilities

--- restify/accounts/urls.py
+++ restify/accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.CreateUser.as_view(), name='signup')
+    path('signup/', views.CreateUser.as_view(), name='signup'),
+    path('view/<pk>', views.ViewProfile.as_view(), name='view'),
 ]
--- restify/accounts/views.py
+++ restify/accounts/views.py
@@ -1,13 +1,19 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework import generics
 
 from accounts.models import User
-from accounts.serializers import UserCreationSerializer
+from accounts.serializers import UserCreationSerializer, UserSerializer
 
 
 # Create your views here.
-class CreateUser(CreateAPIView):
+class CreateUser(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreationSerializer
+
+
+class ViewProfile(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 
 
