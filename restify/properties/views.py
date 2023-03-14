31d710131873269ab35c9d63efb3929056beef32
Goutam112 <goutams112345@gmail.com>
2023-03-14 01:06:24 -0400

    added property filtering and ordering feature

--- restify/properties/views.py
+++ restify/properties/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from django import forms
 
 from accounts.models import User
 from properties.models import Property
@@ -65,9 +66,51 @@ class RetrieveAllPropertiesView(PropertyView, generics.ListAPIView):
     permission_classes = []  # Doesn't require login to access this view
 
     def get_queryset(self):
-        filtered_properties = Property.objects.all()
+        properties = Property.objects.all()
 
-        return filtered_properties
+        # FILTERING PROPERTIES DATA
+        country_filter = forms.CharField(required=False).clean(self.request.GET.get('country', None))
+        min_price_filter = forms.IntegerField(required=False).clean(self.request.GET.get('minPrice', None))
+        max_price_filter = forms.IntegerField(required=False).clean(self.request.GET.get('maxPrice', None))
+        num_guests_filter = forms.IntegerField(required=False).clean(self.request.GET.get('numGuests', None))
+
+        if country_filter is not None:
+            properties = properties.filter(country__iexact=country_filter)
+        if min_price_filter is not None and max_price_filter is not None and min_price_filter > max_price_filter:
+            print("minPrice not lte maxPrice")
+        else:
+            if min_price_filter is not None:
+                properties = properties.filter(nightly_price__gte=min_price_filter)
+            if max_price_filter is not None:
+                properties = properties.filter(nightly_price__lte=int(max_price_filter))
+        if num_guests_filter is not None:
+            if num_guests_filter <= 0:
+                print("minGuests has to be greater than 0")
+            else:
+                properties = properties.filter(max_num_guests__gte=int(num_guests_filter))
+
+        # ORDERING PROPERTIES DATA
+        num_beds_order = self.request.GET.get('bedsOrder', None)
+        if num_beds_order is not None:
+            if 'asc' in num_beds_order.lower():
+                num_beds_order = 'num_beds'
+            if 'desc' in num_beds_order.lower():
+                num_beds_order = '-num_beds'
+            else:
+                print('bedsOrder: (ASC/DESC) only')
+            properties.order_by(num_beds_order)
+        price_order = self.request.GET.get('priceOrder', None)
+        if price_order is not None:
+            if 'asc' in price_order.lower():
+                price_order = 'nightly_price'
+            elif 'desc' in price_order.lower():
+                price_order = '-nightly_price'
+            else:
+                print("priceOrder: (ASC/DESC) only")
+            properties.order_by(price_order)
+
+        return properties
+
 
 
 class RetrieveUserPropertiesView(PropertyView, generics.ListAPIView):
