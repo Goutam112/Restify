b0b49c6c3dc43e6f0358ffb65f7f417f050bc0c3
Jason <jasonw2910@gmail.com>
2023-04-04 16:03:49 -0400

    Add browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+// import ViewProfile from './accounts/pages/viewProfile';
+// import EditProfile from './accounts/pages/editProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route path='accounts'>
+            {/* <Route path='profile/view/:userID' element={<ViewProfile />} />
+            <Route path='profile/edit' element={<EditProfile />} /> */}
+          </Route>
+          { 
+            // TODO: Add remaining routes here. 
+          }
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
