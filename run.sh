72999319624fe0153630c92c84c26e636faed60e
Kevin Wang <76802978+SirRAL@users.noreply.github.com>
2023-04-14 22:48:04 -0400

    Amend run and startup

--- run.sh
+++ run.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+source ~/.nvm/nvm.sh
+
+source ~/.profile
+
+source ~/.bashrc
+
+nvm use 18;
+
+cd frontend;
+
+npm install;
+
+npm start &
+
+cd ../backend/restify;
+
 source ./venv/bin/activate;
 
-python3 ./backend/manage.py makemigrations;
+python3 ./manage.py makemigrations;
 
-python3 ./backend/manage.py migrate;
+python3 ./manage.py migrate;
 
-python3 ./backend/manage.py runserver;
+python3 ./manage.py runserver;
--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd backend/restify;
+
 sudo apt install python3-pip;
 
 sudo apt install python3.8-venv;
@@ -32,5 +34,16 @@ pip install djangorestframework-simplejwt;
 pip install django-apscheduler;
 
 pip install django-cors-headers;
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+source ~/.nvm/nvm.sh
+
+source ~/.profile
+
+source ~/.bashrc
+
+nvm install 18;
+
 
 
